b758545840e31513ee52dab0246c1d4fa2647415
Mohamed Gharsallah <notyet94@gmail.com>
2016-11-20 16:31:13 -0500

    Update vagrant.ps1

--- vagrant.ps1
+++ vagrant.ps1
@@ -11,17 +11,18 @@ echo "  config.vm.box = ""centos""" >> vagrantfile
 echo "  config.ssh.insert_key = false" >> vagrantfile
 echo "  config.ssh.private_key_path = [""~/.ssh/id_rsa"",  ""~/.vagrant.d/insecure_private_key""]" >> vagrantfile
 echo "  config.vm.provision ""file"", source: ""~/.ssh/id_rsa.pub"", destination: ""~/.ssh/authorized_keys""" >> vagrantfile
-echo "  config.vm.provision ""shell"", inline: ""sudo yum -y update""" >> vagrantfile
-echo "  config.vm.provision ""shell"", inline: ""sudo timedatectl set-timezone EST""" >> vagrantfile
+echo "  config.vm.provision ""shell"", inline: <<-SHELL" >> vagrantfile
+echo "    sudo yum -y update" >> vagrantfile
+echo "    sudo timedatectl set-timezone EST" >> vagrantfile
 # SET SWAPPINESS TO 0 TO USE RAM RATHER THAN SWAP
-echo "  config.vm.provision :shell, inline: ""sudo sysctl vm.swappiness=0""" >> vagrantfile
-echo "  config.vm.provision :shell, inline: ""echo ""vm.swappiness=0"" | sudo tee --append /etc/sysctl.conf""" >> vagrantfile
-
+echo "    sudo sysctl vm.swappiness=0" >> vagrantfile
+echo "    echo ""vm.swappiness=0"" | sudo tee --append /etc/sysctl.conf" >> vagrantfile
+echo "  SHELL" >> vagrantfile
 #Here you can add any provionning shell command or script
 #echo "	config.vm.provision :shell, path: ""Provision-script.sh""" >> vagrantfile
 
 echo "  end" >> vagrantfile
-	
+echo "end" >> vagrantfile	
 # Start the old vagrant Box
 vagrant up
 
